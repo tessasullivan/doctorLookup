7f2ad0e16da2c08fb99f10915a17fe2e7dfbca8f
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-06-07 16:37:57 -0700

    refactored displayDoctorsByName, now returns website, accepting new patients

--- src/Doctor.js
+++ src/Doctor.js
@@ -1,9 +1,10 @@
+const betterDoctor = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.exports.apiKey}`;
+
 export class Doctor {
   getDoctorByName(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=3592ed2c785d1f9ddfffcc104267bf81&name=${name}&location=47.608013,-122.335167,50`;
-      // let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.exports.apiKey}&name=${name}&location=47.608013,-122.335167,50`;
+      let url = `${betterDoctor}&name=${name}&location=47.608013,-122.335167,50&limit=50`;
 
       request.open("GET", url, true);
       request.send();
@@ -14,7 +15,7 @@ export class Doctor {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
     });
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,42 +4,61 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Doctor } from './Doctor';
 
-$().ready(function(){
-  $("#nameSearch").submit(function(event) {
+let doctors = []; 
 
+function extractInfo(practice){
+  let street2 = practice.visit_address.street2 || "";
+  let website = practice.website || "";
+  let accepting;
+  if (practice.accepts_new_patients) {
+    accepting = "Yes";
+  } else {
+    accepting = "No";
+  }
+
+  doctors.push({name: practice.name,
+    street: practice.visit_address.street,
+    street2: street2,
+    city:  practice.visit_address.city,
+    state: practice.visit_address.state,
+    zip: practice.visit_address.zip,
+    phone: practice.phones[0].number,
+    website: website,
+    accepting: accepting});
+}
+
+function displayDoctorsByName(name){
+  let doctorList = new Doctor();
+  let promise = doctorList.getDoctorByName(name);
+  
+  promise.then(function(response) {
+    $("#doctorList").show();
+    $('tr:not(#tableHeader)').empty();
+
+    let list = JSON.parse(response);
+
+    list.data.forEach(function(result) {
+      result.practices.forEach(function(practice) {
+        extractInfo(practice);
+      });    
+    });
+
+    doctors.forEach(function(doctor) {
+      $("#doctorList").append(`<tr><td>${doctor.name}</td><td>${doctor.street} ${doctor.street2} ${doctor.city} ${doctor.state} ${doctor.zip}</td>
+        <td>${doctor.phone}</td><td>${doctor.website}</td><td>${doctor.accepting}</td>`);
+    });
+  }, function(error) {
+    $("#results").append(`There was an error processing your request: ${error}`);
+  });  // end promise
+
+}
+
+$().ready(function(){
+  $("#nameSearch").submit(function() {
+    
     event.preventDefault();
     let inputtedName = $("#nameLookup").val();
     $("#nameLookup").val("");
-    let doctorList = new Doctor();
-    let uniqueDoctors = [];
-    
-    let promise = doctorList.getDoctorByName(inputtedName);
-    
-    promise.then(function(response) {
-      $("#results").empty();
-
-      let list = JSON.parse(response);
-      list.data.forEach(function(doctor) {
-        doctor.practices.forEach(function(practice) {
-          // Weed out duplicates
-          let street2 = practice.visit_address.street2 || "";
-          uniqueDoctors.push({name: practice.name,
-            street: practice.visit_address.street,
-            street2: street2,
-            city:  practice.visit_address.city,
-            state: practice.visit_address.state,
-            zip: practice.visit_address.zip});
-          // $("#results").append(`<li>${practice.name} ${practice.visit_address.street} ${street2}
-          // ${practice.visit_address.city} ${practice.visit_address.state} ${practice.visit_address.zip}</li>`);
-        });    
-      })
-    }, function(error) {
-      console.error(`There was an error processing your request: ${error}`);
-    });  // end promise
-
-    console.log(uniqueDoctors);
-    uniqueDoctors.forEach(function(doctor) {
-      $("#results").append(`<li>${doctor.name} ${doctor.street} ${doctor.street2} ${doctor.city} ${doctor.state} ${doctor.zip}</li>`);
-    })
-  }); // end submit function
-})
+    displayDoctorsByName(inputtedName);
+  }); 
+});
